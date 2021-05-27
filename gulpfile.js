0642cba649c4e186ad5aa0dfe77fce87bf3e39f8
Xronik <xronik2000@gmail.com>
2021-05-27 05:35:49 +0300

    third commit

--- gulpfile.js
+++ gulpfile.js
@@ -1,24 +1,40 @@
 const { src, dest, series, watch } = require('gulp')
 const sass = require('gulp-sass')
 const csso = require('gulp-csso')
-const include = require('gulp-file-include')
 const htmlmin = require('gulp-htmlmin')
 const del = require('del')
 const concat = require('gulp-concat')
 const autoprefixer = require('gulp-autoprefixer')
 const sync = require('browser-sync').create()
 const pug = require('gulp-pug')
+const rename = require("gulp-rename");
+const uglify = require('gulp-uglify-es').default;
+const imagemin = require('gulp-imagemin');
+const ttf2woff = require('gulp-ttf2woff');
+const ttf2woff2 = require('gulp-ttf2woff2');
 
-// function html() {
-//   return src('dist/**.html')
-//     .pipe(htmlmin({
-//       collapseWhitespace: true
-//     }))
-// }
+function html() {
+  return src('src/**.html')
+    .pipe(htmlmin({
+      collapseWhitespace: true
+    }))
+    .pipe(dest('dist'))
+}
+
+function fonts() {
+  src('src/fonts/*.ttf')
+    .pipe(ttf2woff())
+    .pipe(dest('dist/fonts'))
+  return src('src/fonts/*.ttf')
+    .pipe(ttf2woff2())
+    .pipe(dest('dist/fonts'))
+}
 
 function pugCompile() {
   return src('src/**.pug')
-    .pipe(pug())
+    .pipe(pug({
+      pretty: true
+    }))
     .pipe(dest('src'))
 }
 
@@ -26,26 +42,59 @@ function scss() {
   return src('src/scss/**.scss')
     .pipe(sass())
     .pipe(autoprefixer({
-      browsers: ['last 2 versions']
+      overrideBrowserslist: ['last 2 versions'],
+      cascade: true
     }))
-    // .pipe(csso())
-    .pipe(concat('index.css'))
-    .pipe(dest('dist'))
     .pipe(dest('src/scss'))
+}
+
+function cssMin() {
+  return src('src/scss/**.css')
+    .pipe(concat('style.css'))
+    .pipe(csso())
+    .pipe(rename({
+      extname: ".min.css"
+    }))
+    .pipe(dest('dist/css'))
+}
+
+function jsMin() {
+  return src('src/js/**.js')
+    .pipe(concat('main.js'))
+    .pipe(uglify())
+    .pipe(rename({
+      extname: ".min.js"
+    }))
+    .pipe(dest('dist/js'))
+}
+
+function images() {
+  return src('src/images/*')
+    .pipe(imagemin({
+      progressive: true,
+      svgoPlugins: [{ removeViewBox: false }],
+      interlaced: true,
+      optimizationLevel: 3
+    }))
+    .pipe(dest('dist/images'))
 }
 
 function clear() {
   return del('dist')
 }
 
-function serve() {
+function server() {
   sync.init({
-    server: './src'
+    server: './dist'
   })
   watch('src/**.pug', series(pugCompile)).on('change', sync.reload)
+  watch('src/**.html', series(html)).on('change', sync.reload)
+
   watch('src/scss/**.scss', series(scss)).on('change', sync.reload)
+  watch('src/scss/**.css', series(cssMin)).on('change', sync.reload)
+  watch('src/js/**.js', series(jsMin)).on('change', sync.reload)
 }
 
-exports.build = series(clear, scss)
-exports.serve = series(scss, pugCompile, serve)
+exports.build = series(clear, cssMin, html, fonts, jsMin, images)
+exports.start = server
 exports.clear = clear
--- src/js/main.js
+++ src/js/main.js
@@ -1,30 +1,7 @@
-const inputEmail = document.querySelector('#email')
-const inputPassword = document.querySelector('#password')
-const currenceConverterResult = document.querySelector('.converter-result')
-let btn = document.querySelectorAll('.converter-btn')
+const converterResult = document.querySelector('.converter-result')
+const btnsEl = document.querySelectorAll('.converter-btn')
 
-btn.forEach(el => {
-  el.addEventListener('click', (e) => {
-    let valueTo = e.currentTarget.dataset.valueto
-    let valueFrom = e.currentTarget.dataset.valuefrom
-    fetch(`https://currency-exchange.p.rapidapi.com/exchange?to=${valueTo}&from=${valueFrom}`, {
-      "method": "GET",
-      "headers": {
-        "x-rapidapi-key": "7a998b5da0msh2aeaf7ecb754373p18a88fjsnf26dc7e7c2c3",
-        "x-rapidapi-host": "currency-exchange.p.rapidapi.com"
-      }
-    })
-      .then(response => {
-        return response.json();
-      })
-      .then(data => {
-        currenceConverterResult.innerHTML = data
-      })
-      .catch(err => {
-        console.error(err);
-      });
-  })
-})
+// Validation and submit form
 
 let validateForms = (selector) => {
   new window.JustValidate(selector, {
@@ -55,3 +32,28 @@ let validateForms = (selector) => {
 }
 
 validateForms('.form')
+
+// Fetch queries for currency converter
+
+btnsEl.forEach(el => {
+  el.addEventListener('click', (e) => {
+    let valueTo = e.currentTarget.dataset.valueto
+    let valueFrom = e.currentTarget.dataset.valuefrom
+    fetch(`https://currency-exchange.p.rapidapi.com/exchange?to=${valueTo}&from=${valueFrom}`, {
+      "method": "GET",
+      "headers": {
+        "x-rapidapi-key": "7a998b5da0msh2aeaf7ecb754373p18a88fjsnf26dc7e7c2c3",
+        "x-rapidapi-host": "currency-exchange.p.rapidapi.com"
+      }
+    })
+      .then(response => {
+        return response.json();
+      })
+      .then(data => {
+        converterResult.innerHTML = data
+      })
+      .catch(err => {
+        console.error(err);
+      });
+  })
+})
